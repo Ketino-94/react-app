d7da01f2dc01c15d4a6214b40df4147c43144043
Ketino <ketino_gr@ukr.net>
2019-05-23 14:45:43 +0300

    add poject

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoList from './components/todo-list';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+
+const App = () => {
+
+  const todoData = [
+    {label: 'Drink Cofee', important: false},
+    {label: 'Make Awesome App', important: true},
+    {label: 'Have a lunch', important: false},
+  ];
+
+  return(
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList todos={todoData}/>
+    </div>
+  )
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
